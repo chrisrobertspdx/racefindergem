2205ee99dac1dec84d98b8ad2170089337b4457e
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-26 23:01:52 +0000

    stubbed CLI controller

---
+++ race_finder/lib/race_finder/cli.rb
@@ -0,0 +1,11 @@
+class RaceFinder::CLI
+  #attr_accessor :facility
+  BASE_URL = 'http://www.racecenter.org'
+
+  def run
+    puts "Hello. I would like to help you find a race."
+    #make_recs
+    #add_attributes_to_recs
+    #display_recs
+  end
+end
