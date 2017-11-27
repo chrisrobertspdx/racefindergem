78e937a7336670a6bc9c61369276a31a3f4c4a7f
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-27 16:53:32 +0000

    fixed connection problem

--- race_finder/lib/race_finder/cli.rb
+++ race_finder/lib/race_finder/cli.rb
@@ -1,12 +1,12 @@
 class RaceFinder::CLI
   attr_accessor :calendar
-  BASE_URL = 'https://www.racecenter.org/race-calendar/'
+  BASE_URL = 'https://www.racecenter.com/race-calendar/'
 
   def run
-    puts "Hello. I would like to help you find a bike race."
+    puts "Hello. I would like to help you find a race."
     add_races_to_calendar
     list_races
-    #get / add races
+    menu
     #ask for selection
     #add_attributes_to_races
     #display_recs
@@ -18,10 +18,7 @@ class RaceFinder::CLI
     race_data.each {|d|
       self.calendar.races << RaceFinder::Race.new(d)
     }
-    #scrape races
-    #iterate and add each to calendar
-    #self.calendar.races << RaceFinder::Race.new(name: "Crusade 1")
-    #self.calendar.races << RaceFinder::Race.new(name: "Crusade 2")
+
   end
     
   def list_races
@@ -33,7 +30,7 @@ class RaceFinder::CLI
   def menu
     input = ""
     while input != "exit"
-      puts "What would you like to do?"
+      puts "Which race looks interesting? "
       input = gets.strip
       case input
       when "list"
--- race_finder/lib/race_finder/race.rb
+++ race_finder/lib/race_finder/race.rb
@@ -1,5 +1,5 @@
 class RaceFinder::Race
-    attr_accessor :name, :type, :date, :location, :detail, :venue
+    attr_accessor :name, :type, :date, :location, :description, :venue, :url
     def initialize(attr)
         attr.each {|key, value| self.send(("#{key}="), value)}
     end
@@ -9,9 +9,8 @@ class RaceFinder::Race
     def show_details
        puts "#{self.type.upcase}"
        puts "#{self.name} | #{self.date} | #{self.location}"
-       puts "Venue: #{self.venue}"
        puts "---------------"
-       puts self.detail
+       puts self.description
        puts "---------------"
     end
 end
