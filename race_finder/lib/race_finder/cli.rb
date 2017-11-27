b8f087b678ef7e35bb5273cd599ece25decb599b
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-27 01:43:19 +0000

    added calendar and race classes

--- race_finder/lib/race_finder/cli.rb
+++ race_finder/lib/race_finder/cli.rb
@@ -1,9 +1,9 @@
 class RaceFinder::CLI
-  #attr_accessor :facility
+  #attr_accessor :calendar
   BASE_URL = 'http://www.racecenter.org'
 
   def run
-    puts "Hello. I would like to help you find a race."
+    puts "Hello. I would like to help you find a bike race."
     #make_recs
     #add_attributes_to_recs
     #display_recs
